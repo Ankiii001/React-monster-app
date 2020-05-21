c8e8388b8adee46e8660915673bbfea17592e0a0
Ankit Panwar <ankit.panwar@nagarro.com>
2020-05-22 02:49:55 +0530

    Updates

---
+++ precache-manifest.11a0f2b6ffb436877414e7911d82a1ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3f30a0fa48fb5210ca61933c9667728",
+    "url": "/React-monster-app/index.html"
+  },
+  {
+    "revision": "6896e2897ce0c44e5e0e",
+    "url": "/React-monster-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "923dc8103d094f32fd77",
+    "url": "/React-monster-app/static/css/main.72567803.chunk.css"
+  },
+  {
+    "revision": "6896e2897ce0c44e5e0e",
+    "url": "/React-monster-app/static/js/2.4581c688.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-monster-app/static/js/2.4581c688.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "923dc8103d094f32fd77",
+    "url": "/React-monster-app/static/js/main.e04f29f1.chunk.js"
+  },
+  {
+    "revision": "77de1bb7a3ae5286ed2f",
+    "url": "/React-monster-app/static/js/runtime-main.231a8514.js"
+  }
+]);
